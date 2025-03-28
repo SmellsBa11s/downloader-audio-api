af6e6379b1fae2a2532b9a65a420b3a4799d7ea9
Лень Матвей <matveylenvital@yandex.ru>
2025-03-29 00:29:34 +0300

    update supervisor

--- src/routers/supervisor.py
+++ src/routers/supervisor.py
@@ -1,7 +1,8 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from src.core.dependencies import get_admin_user
 from src.models import User
-from src.schemas import UserInfo, UpdateUserInfo
+from src.schemas import UserInfo, UpdateUserInfo, AudioFullInfo
 from src.service import SupervisorService
 
 
@@ -9,27 +10,49 @@ router = APIRouter()
 
 
 @router.get("/{user_id}")
-async def get_user(
+async def get_user_info(
     user_id: int,
     supervisor_service: SupervisorService = Depends(),
     user: User = Depends(get_admin_user),
 ) -> UserInfo:
     return await supervisor_service.get_user(user_id)
 
+
+@router.get("/{user_id}/audio")
+async def get_user_audio(
+    user_id: int,
+    supervisor_service: SupervisorService = Depends(),
+    user: User = Depends(get_admin_user),
+) -> List[AudioFullInfo]:
+    return await supervisor_service.get_user_audio(user_id)
+
+
 @router.put("/{user_id}")
 async def update_user(
     user_id: int,
     user_info: UpdateUserInfo,
     supervisor_service: SupervisorService = Depends(),
-    user: User = Depends(get_admin_user)
+    user: User = Depends(get_admin_user),
 ) -> UserInfo:
     return await supervisor_service.update_user(user_id=user_id, update_data=user_info)
+
 
 @router.delete("/{user_id}")
 async def delete_user(
     user_id: int,
     full_delete: bool = False,
     supervisor_service: SupervisorService = Depends(),
-    user: User = Depends(get_admin_user)
+    user: User = Depends(get_admin_user),
 ) -> bool | UserInfo:
-    return await supervisor_service.delete_user(user_id=user_id, full_delete=full_delete)
+    return await supervisor_service.delete_user(
+        user_id=user_id, full_delete=full_delete
+    )
+
+
+@router.post("/activate-user/")
+async def activate_user(
+    user_id: int,
+    supervisor_service: SupervisorService = Depends(SupervisorService),
+    user: User = Depends(get_admin_user),
+) -> bool:
+    return await supervisor_service.activate_user(user_id=user_id)
--- src/service/supervisor.py
+++ src/service/supervisor.py
@@ -1,13 +1,13 @@
 from fastapi import Depends
-from src.crud import UserDAO
+from src.crud import UserDAO, AudioDAO
 from src.models import User
-from src.schemas import UserInfo, UpdateUserInfo
+from src.schemas import UserInfo, UpdateUserInfo, AudioFullInfo
 
 
 class SupervisorService:
-    def __init__(self, user_dao: UserDAO = Depends()):
+    def __init__(self, user_dao: UserDAO = Depends(), audio_dao: AudioDAO = Depends()):
         self._user_dao = user_dao
-
+        self._audio_dao = audio_dao
 
     async def process_user_info(self, user: User) -> UserInfo:
         return UserInfo(
@@ -15,28 +15,51 @@ class SupervisorService:
             yandex_id=user.yandex_id,
             first_name=user.first_name,
             last_name=user.last_name,
-            email=user.email
+            email=user.email,
+            is_active=user.is_active,
+            is_supervisor=user.is_supervisor,
+            created_at=user.created_at,
+            updated_at=user.updated_at,
         )
+
     async def get_user(self, user_id: int) -> UserInfo:
         result = await self._user_dao.find_one(id=user_id)
         return await self.process_user_info(result)
 
     async def update_user(self, user_id: int, update_data: UpdateUserInfo) -> UserInfo:
         update_data = update_data.dict(exclude_none=True)
+        user = await self._user_dao.find_one(id=user_id, is_active=True)
 
         if update_data:
-            user_list = await self._user_dao.update(
-                model_id=user_id, **update_data
-            )
+            user_list = await self._user_dao.update(model_id=user_id, **update_data)
             user = user_list[0]
-        else:
-            user = await self._user_dao.find_one(id=user_id)
 
         return await self.process_user_info(user)
-    
+
     async def delete_user(self, user_id: int, full_delete: bool = False) -> bool:
+        await self._user_dao.find_one(id=user_id, is_active=True)
         if full_delete:
             await self._user_dao.delete(model_id=user_id)
         else:
             await self._user_dao.update(model_id=user_id, is_active=False)
         return True
+
+    async def activate_user(self, user_id: int) -> bool:
+        await self._user_dao.update(model_id=user_id, is_active=True)
+        return True
+
+    async def get_user_audio(self, user_id: int) -> list[AudioFullInfo]:
+        """Получает список аудио файлов пользователя."""
+        await self._user_dao.find_one(id=user_id, is_active=True)
+        audio_files = await self._audio_dao.find_all(user_id=user_id, is_deleted=False)
+        return [
+            AudioFullInfo(
+                audio_id=audio.id,
+                filename=audio.filename,
+                user_id=audio.user_id,
+                path=audio.path,
+                size=audio.size,
+                created_at=audio.created_at,
+            )
+            for audio in audio_files
+        ]
